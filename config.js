39968ccaaa2596fb10fcfd4a3e2366a9fcd1cfff
Soo-minJeon <smj8554@naver.com>
2022-03-11 10:04:08 +0900

    모듈화 각자 맡은 부분 합침

---
+++ config.js
@@ -0,0 +1,34 @@
+// 설정 정보
+
+module.exports = {
+    server_port : 3000,
+    db_url : "mongodb://127.0.0.1:27017/local",
+    db_schemas : [
+        {file : './UserSchema', 
+        collection : 'UserCollection', 
+        schemaName : "UserSchema", 
+        modelName : "UserModel"},
+
+        {file : './WatchSchema', 
+        collection : 'WatchCollection', 
+        schemaName : "WatchSchema", 
+        modelName : "WatchModel"},
+
+        {file : './RoomSchema', 
+        collection : 'RoomCollection', 
+        schemaName : "RoomSchema", 
+        modelName : "RoomModel"}
+    ],
+    route_info: [
+        {file:'./routes/user', path:'/login', method:'login', type:'post'},
+        {file:'./routes/user', path:'/signup', method:'signUp', type:'post'},
+        {file:'./routes/user', path:'/watchlist', method:'watchlist', type:'post'},
+        {file:'./routes/user', path:'/watchresult', method:'watchresult', type:'post'},
+        {file:'./routes/user', path:'/recommend1', method:'recommend1', type:'post'},
+        {file:'./routes/user', path:'/recommend2', method:'recommend2', type:'post'},
+        {file:'./routes/user', path:'/enterroom', method:'enterRoom', type:'post'},
+        {file:'./routes/user', path:'/email', method:'email', type:'post'},
+        {file:'./routes/user', path:'/makeRoom', method:'makeRoom', type:'post'},
+        {file:'./routes/user', path:'/logout', method:'logout', type:'post'}
+    ]
+}

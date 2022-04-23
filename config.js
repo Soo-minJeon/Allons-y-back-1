2b3efe05099cd8ba78a14f46cfbc934b7ea86a7d
smj8554 <smj8554@duksung.ac.kr>
2022-04-23 21:37:43 +0900

    회원가입, 로그인, 감상시작 노드 코드 작성 및 csv파일에서 영화 데이터 가져오기

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     server_port : 3000,
-    db_url : "mongodb://127.0.0.1:27017/local",
+    db_url : "mongodb://localhost/local",
     db_schemas : [
         {file : './UserSchema', 
         collection : 'UserCollection', 
---
+++ findByTitle_fromMovieData.py
@@ -0,0 +1,33 @@
+import pandas as pd # pandas
+import sys
+
+def process(title):
+
+        # 생략없이 출력
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.width', 300)
+
+        # csv 파일 불러오기
+        movies = pd.read_csv(
+                '/Users/jeonsumin/PycharmProjects/allonsy-recommend/madeBySooMin#1_similarUser/movie_info.csv',
+                low_memory=False)
+        movies.columns = ['id', 'original_title', 'genres','runtime', 'release_date', 'actor', 'poster_path']
+
+        # 제목으로 정보 찾기
+        find_row = movies.index[(movies['original_title'] == title)]
+
+        # 장르 문자열 처리
+        genres = movies.loc[find_row[0]]['genres']
+        genres = genres.replace('[', '').replace(']', '').split('}, {')
+        genres_after = []
+
+        for i in range(len(genres)):
+                genre = genres[i].split(', ')[1].split(': ')[1].replace("'", '').replace("}", '')
+                genres_after.append(genre)
+
+        print(movies.loc[find_row[0]]['original_title'],'|',  genres_after, '|', movies.loc[find_row[0]]['poster_path'])
+
+
+if __name__ == '__main__':
+    process(sys.argv[1])

0b74cccc4e4292aed02c4e092862800997fee717
smj8554 <smj8554@duksung.ac.kr>
2022-08-28 00:20:33 +0900

    혼자보기 후 정보전송형형식 수정

--- routes/user.js
+++ routes/user.js
@@ -1558,8 +1558,8 @@ var watchAloneEnd = async function(req, res){
         await ratingUpdate();
 
         var objToSend = {
-          genres : "movie_genre",
-          poster : "movie_poster"
+          genres : movie_genre,
+          poster : movie_poster
         }
         res.status(200).send(JSON.stringify(objToSend));
         console.log('----------------------------------------------------------------------------')
